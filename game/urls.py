d383d8b628ab34a5e4d8edb6f0198ceb9949d364
apkallum <apkallum@protonmail.com>
2019-08-09 16:14:01 -0400

    change game.state default value to json

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
+from django.contrib.auth.views import LoginView
+
 from .views import GameDetailView
+from .views import GameCreateView
 
 urlpatterns = [
+    path('create/', GameCreateView.as_view(), name="create"),
     path('<uuid:pk>', GameDetailView.as_view(), name='GameDetailView'),
+      path('login/', LoginView.as_view(), name="login"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Game
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Game, Move
 
 class GameDetailView(generic.DetailView):
     model = Game
 
+class GameCreateView(LoginRequiredMixin, generic.edit.CreateView):
+    model = Game
+    fields = ['player2']
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['player1'] = self.request.user.username
