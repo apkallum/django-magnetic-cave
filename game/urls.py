78bea59542e8a901c8d07376abd0fd72b434df87
apkallum <apkallum@protonmail.com>
2019-08-15 03:51:25 -0400

    clean up the views and urls

--- game/urls.py
+++ game/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import GamePlayView, GameCreateView
+from .views import GamePlayView
 from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('create/', GameCreateView.as_view(), name="create"),
     path('<str:id>', GamePlayView, name='GamePlayView'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render,redirect, reverse
+from django.shortcuts import render,redirect, reverse, get_object_or_404
 from django.views import generic
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Game, Move
 from .forms import MoveForm
 
-@login_required
 def index(request):
     player_id = request.user.id
     form = MoveForm(initial={'game': game.id, 'player_id': player_id})
@@ -19,23 +17,18 @@ def index(request):
 @login_required
 def GamePlayView(request, id):
     game_id = id
-    game = Game.objects.get(id=game_id)
+    game = get_object_or_404(Game, id=game_id)
     player_id = request.user.id
     form = MoveForm(request.POST)
+    if not game.player2_id:
+        game.player2_id = request.user.id
     if request.method == "POST":
         move_coordinates = request.POST.get('move_coordinates')
         Move.objects.create(game_id = game.id, player_id = player_id, move_coordinates = move_coordinates)
-        return redirect('./' + game.id)
+        return redirect('GamePlayView', game.id)
     context = {
         'game': game,
         'form': form
     }
     return render(request, "game/game.html", context)
 
-class GameCreateView(LoginRequiredMixin, generic.edit.CreateView):
-    model = Game
-    fields = ['player2']
-
-    def get_initial(self):
-        initial = super().get_initial()
-        initial['player1'] = self.request.user.username
